e1fc4158e1dd7a677203db795babf5a2d9d9b05d
SamiFawcett <samif7466@afsenyc.org>
2019-03-16 18:34:22 -0400

    server user access user creation

--- cleanupcrew-app/server/routes/users.js
+++ cleanupcrew-app/server/routes/users.js
@@ -1,22 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var mongoUtil = require('../database/db.js');
 
+mongoUtil.connectToServer(function(err){
+
+});
+
+//assume id is given in req
 router.get('/user', function(req, res, next){
   // read req to get a user id
-
+  var id_token = req.query['id_token'];
   // lookup the id in the database
-
-  // vvvv
-  res.send('lodddl'); // send any data from the database
+  var db = mongoUtil.getDb();
+  // var cursor = db.collection('cc_col').find({id_token:id_token});
+  var result_cursor = db.collection('cc_col').findOne({id_token: id_token});
+  result_cursor.then(function(result){
+    res.send(result);
+  })
 })
-
-router.post('/userfff', function(req, res, next){
+// assume /user?idtoken=x&name=x&email=x
+router.post('/user', function(req, res, next){
   // read req to get a username or password
-
+  var id_token = req.query['id_token'];
+  var name = req.query['name'];
+  var email = req.query['email'];
   // create the user object in the database
+  var user = {
+    "id_token":id_token,
+    "name":name,
+    "email":email
+  };
+
+  var db = mongoUtil.getDb();
 
   // vvvv
-  res.send('lol'); // send any data from the database, or just send a succuess or conformatoin
+  // send any data to the database, or just send a succuess or conformatoin
+  var c = db.collection('cc_col').insertOne(user);
+  console.log("successfully added user to database");
 })
 
 module.exports = router;
