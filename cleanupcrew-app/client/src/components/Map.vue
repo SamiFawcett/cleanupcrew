ceaff4261c32ca172d26b8492fed581567a2b0b7
Quinn McHugh <qpmchugh@outlook.com>
2019-03-17 03:22:55 -0400

    Removed script

--- cleanupcrew-app/client/src/components/Map.vue
+++ cleanupcrew-app/client/src/components/Map.vue
@@ -11,39 +11,3 @@
     </ion-page>
   </ion-app>
 </template>
-
-<script>
-  marker = new google.maps.Marker(
-  {
-      map:map,
-      draggable:true,
-      animation: google.maps.Animation.DROP,
-      position: results[0].geometry.location
-  });
-  google.maps.event.addListener(marker, 'dragend', function()
-  {
-      geocodePosition(marker.getPosition());
-  });
-
-  function geocodePosition(pos)
-  {
-     geocoder = new google.maps.Geocoder();
-     geocoder.geocode
-      ({
-          latLng: pos
-      },
-          function(results, status)
-          {
-              if (status == google.maps.GeocoderStatus.OK)
-              {
-                  $("#mapSearchInput").val(results[0].formatted_address);
-                  $("#mapErrorMsg").hide(100);
-              }
-              else
-              {
-                  $("#mapErrorMsg").html('Cannot determine address at this location.'+status).show(100);
-              }
-          }
-      );
-  }
-  </script>
