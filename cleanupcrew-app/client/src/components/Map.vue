63379a4a23197c70a5e136bde1baea4db252e2a9
Quinn McHugh <qpmchugh@outlook.com>
2019-03-17 03:06:09 -0400

    Test

--- cleanupcrew-app/client/src/components/Map.vue
+++ cleanupcrew-app/client/src/components/Map.vue
@@ -11,3 +11,31 @@
     </ion-page>
   </ion-app>
 </template>
+
+<script>
+  showMap(10, 10, "Glassboro, NJ");
+
+  function showMap(lat,lang,address) {
+    var myLatLng = {lat: lat, lng: lang};
+
+    var map = new google.maps.Map(document.getElementById('map_canvas'), {
+      zoom: 17,
+      center: myLatLng
+    });
+
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: address,
+      draggable:true,
+    });
+
+    google.maps.event.addListener(marker, 'dragend', function(marker){
+        var latLng = marker.latLng;
+        currentLatitude = latLng.lat();
+        currentLongitude = latLng.lng();
+        jQ("#latitude").val(currentLatitude);
+        jQ("#longitude").val(currentLongitude);
+     });
+  }
+</script>
