a21d6bc734b5b1fc4e217fb489703fbbd6d8110d
Quinn McHugh <qpmchugh@outlook.com>
2019-03-17 02:24:26 -0400

    Added map to Map.vue

--- cleanupcrew-app/client/src/components/Map.vue
+++ cleanupcrew-app/client/src/components/Map.vue
@@ -11,3 +11,39 @@
     </ion-page>
   </ion-app>
 </template>
+
+<script>
+  Vue.use(VueGoogleMaps, {
+    load: {
+      key: 'AIzaSyDf43lPdwlF98RCBsJOFNKOkoEjkwxb5Sc'
+    },
+    // Demonstrating how we can customize the name of the components
+    installComponents: false,
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    Vue.component('google-map', VueGoogleMaps.Map);
+    Vue.component('google-marker', VueGoogleMaps.Marker);
+
+    new Vue({
+      el: '#root',
+      data: {
+        center: {
+          lat: 10.0,
+          lng: 10.0
+        },
+        markers: [{
+          position: {
+            lat: 10.0,
+            lng: 10.0
+          }
+        }, {
+          position: {
+            lat: 11.0,
+            lng: 11.0
+          }
+        }]
+      },
+    });
+  });
+</script>
