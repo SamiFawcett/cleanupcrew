ac8451b26208a3c3267dbc7a5c89cfc1cb707c4c
Quinn McHugh <qpmchugh@outlook.com>
2019-03-17 01:55:19 -0400

    Added map

--- cleanupcrew-app/client/src/components/Map.vue
+++ cleanupcrew-app/client/src/components/Map.vue
@@ -3,36 +3,47 @@
     <ion-page class="show-page">
       <ion-header>
         <ion-toolbar>
-          <ion-title>Search Cleanups</ion-title>
+          <ion-title>Specify Cleanup Location</ion-title>
         </ion-toolbar>
       </ion-header>
       <ion-content class="content" padding>
-        <ion-searchbar showCancelButton placeholder="Cleanups"></ion-searchbar>
-        <ion-list>
-          <ion-item v-for="item of items" v-bind:key="item.id">
-            <ion-label @click="goToCleanup(item.id)" full>{{item.name}}</ion-label>
-          </ion-item>
-        </ion-list>
+        <div
       </ion-content>
     </ion-page>
   </ion-app>
 </template>
-
 <script>
-export default {
-  name: 'Search',
-  data() {
-    return {
-      items: [{id: 0, name: "test1"},{id: 1, name: "test2"},{id: 2, name: "test1"},
-              {id: 3, name: "test2"},{id: 4, name: "test1"},{id: 5, name: "test2"},
-              {id: 6, name: "test1"},{id: 7, name: "test2"},{id: 8, name: "test1"},
-              {id: 9, name: "test2"},{id: 10, name: "test1"},{id: 11, name: "test2"}]
-    }
-  },
-  methods: {
-    goToCleanup: function(eventID) {
-      this.$router.push('/event/'+eventID)
-    }
-  }
-}
+  Vue.use(VueGoogleMaps, {
+    load: {
+      key: 'AIzaSyDs-DZDyN0p3S4O9V0K8b7X3GyxfeHaKis'
+    },
+    // Demonstrating how we can customize the name of the components
+    installComponents: false,
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    Vue.component('google-map', VueGoogleMaps.Map);
+    Vue.component('google-marker', VueGoogleMaps.Marker);
+
+    new Vue({
+      el: '#root',
+      data: {
+        center: {
+          lat: 10.0,
+          lng: 10.0
+        },
+        markers: [{
+          position: {
+            lat: 10.0,
+            lng: 10.0
+          }
+        }, {
+          position: {
+            lat: 11.0,
+            lng: 11.0
+          }
+        }]
+      },
+    });
+  });
 </script>
