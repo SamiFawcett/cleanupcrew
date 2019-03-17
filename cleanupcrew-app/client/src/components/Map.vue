9946c6b81865cd8e2343d8367e0a5561d3607006
Quinn McHugh <qpmchugh@outlook.com>
2019-03-17 02:48:08 -0400

    Removed script from Map.vue

--- cleanupcrew-app/client/src/components/Map.vue
+++ cleanupcrew-app/client/src/components/Map.vue
@@ -13,39 +13,3 @@
 </template>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.18/vue.min.js"></script>
-
-<script>
-  Vue.use(VueGoogleMaps, {
-    load: {
-      key: 'AIzaSyDs-DZDyN0p3S4O9V0K8b7X3GyxfeHaKis'
-    },
-    // Demonstrating how we can customize the name of the components
-    installComponents: false,
-  });
-
-  document.addEventListener('DOMContentLoaded', function() {
-    Vue.component('google-map', VueGoogleMaps.Map);
-    Vue.component('google-marker', VueGoogleMaps.Marker);
-
-    new Vue({
-      el: '#root',
-      data: {
-        center: {
-          lat: 10.0,
-          lng: 10.0
-        },
-        markers: [{
-          position: {
-            lat: 10.0,
-            lng: 10.0
-          }
-        }, {
-          position: {
-            lat: 11.0,
-            lng: 11.0
-          }
-        }]
-      },
-    });
-  });
-</script>
