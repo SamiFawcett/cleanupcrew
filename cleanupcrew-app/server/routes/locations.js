30bcec56af27eea2b97c2357e6c7a9294d9c63eb
SamiFawcett <samif7466@afsenyc.org>
2019-03-16 20:53:15 -0400

    updated users.js

---
+++ cleanupcrew-app/server/routes/locations.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var mongoUtil = require('../database/db.js');
+
+mongoUtil.connectToServer(function(err){
+
+});
+
+router.post('/loc', function(req, res, next){
+  var lat = req.query['lat'];
+  var long = req.query['long'];
+  var event_id = req.query['event_id']
+  var user_id = req.query['user_id'];
+  var time_stamp = req.query['time_stamp'];
+  var loc = {
+    "lat":lat,
+    "long":long,
+    "event_id":event_id,
+    "user_id":user_id,
+    "time_stamp":time_stamp
+  };
+  var db = mongUtil.getDb();
+  var c = db.collection('cc_loc').insertOne(loc);
+  console.log("successfully added location to database");
+
+})
+
+//get all locations
+router.get('/loc', function(req, res, next){
+  db.collection('cc_loc').forEach((doc) => {
+    
+  })
+});
--- cleanupcrew-app/server/routes/users.js
+++ cleanupcrew-app/server/routes/users.js
@@ -13,29 +13,30 @@ router.get('/user', function(req, res, next){
   // lookup the id in the database
   var db = mongoUtil.getDb();
   // var cursor = db.collection('cc_col').find({id_token:id_token});
-  var result_cursor = db.collection('cc_col').findOne({id_token: id_token});
+  var result_cursor = db.collection('cc_users').findOne({id_token: id_token});
   result_cursor.then(function(result){
     res.send(result);
   })
 })
+
+
 // assume /user?idtoken=x&name=x&email=x
 router.post('/user', function(req, res, next){
   // read req to get a username or password
   var id_token = req.query['id_token'];
   var name = req.query['name'];
   var email = req.query['email'];
+  var organizer = req.query['organizer'];
   // create the user object in the database
   var user = {
     "id_token":id_token,
     "name":name,
-    "email":email
+    "email":email,
+    "organizer": organizer=='true'
   };
-
   var db = mongoUtil.getDb();
-
-  // vvvv
   // send any data to the database, or just send a succuess or conformatoin
-  var c = db.collection('cc_col').insertOne(user);
+  db.collection('cc_users').insertOne(user);
   console.log("successfully added user to database");
 })
 
