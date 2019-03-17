032c1e4aefcbd4e2869810fdc76eb97a425e35db
Quinn McHugh <qpmchugh@outlook.com>
2019-03-17 02:23:06 -0400

    testing

---
+++ cleanupcrew-app/client/src/components/Map.vue
@@ -0,0 +1,13 @@
+<template>
+  <ion-app>
+    <ion-page class="show-page">
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>Select the location of your cleanup:</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content class="content" padding>
+      </ion-content>
+    </ion-page>
+  </ion-app>
+</template>
